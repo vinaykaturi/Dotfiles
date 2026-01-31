a5f4880a1b6210fe0fe087b7ec0c025bd5e282ca
vinaykaturi <vinaykaturi99@gmail.com>
2026-02-01 00:22:14 +0530

    Initial commit configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+set -U fish_greeting
+fastfetch -c /usr/share/fastfetch/presets/examples/12.jsonc
+
+alias ls="eza -lh"
+alias la="eza -alh"
+alias cls="clear"
+
+end
